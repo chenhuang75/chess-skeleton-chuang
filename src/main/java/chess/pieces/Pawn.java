c96ef48528fa0921df4e24ebcc3ac82c3e575dc3
chenhuang75 <chen.huang.c@gmail.com>
2015-12-05 14:30:29 -0500

    Update Pawn.java

--- src/main/java/chess/pieces/Pawn.java
+++ src/main/java/chess/pieces/Pawn.java
@@ -1,6 +1,11 @@
 package chess.pieces;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import chess.GameState;
 import chess.Player;
+import chess.Position;
 
 /**
  * The Pawn
@@ -14,4 +19,78 @@ public class Pawn extends Piece {
     protected char getIdentifyingCharacter() {
         return 'p';
     }
+
+    /**
+     * get all possible next positions
+     */
+    public List<Position> getNextPositions(Position initPos, GameState state) {
+    	char col;
+    	int row;
+    	List<Position> positions = new ArrayList<Position>();
+    	Position newPos;
+    	
+    	if( getOwner() == Player.White) {
+        	// move up
+        	col = initPos.getColumn();
+        	row = initPos.getRow();
+    		newPos = new Position(col, ++row);
+    		if(isMoveValid(newPos, state) && state.getPieceAt(newPos) == null) {
+    			positions.add(newPos);
+    			if(row == 3) {
+    				newPos = new Position(col, ++row);
+    				if(isMoveValid(newPos, state) && state.getPieceAt(newPos) == null) {
+    					positions.add(newPos);
+    				}
+    			}
+    		}
+    		
+    		// move up left
+        	col = initPos.getColumn();
+        	row = initPos.getRow();
+    		newPos = new Position(--col, ++row);
+    		if(isMoveValid(newPos, state) && state.getPieceAt(newPos) != null) {
+    			positions.add(newPos);
+    		}
+
+    		// move up right
+        	col = initPos.getColumn();
+        	row = initPos.getRow();
+    		newPos = new Position(++col, ++row);
+    		if(isMoveValid(newPos, state) && state.getPieceAt(newPos) != null) {
+    			positions.add(newPos);
+    		}
+    	} else {
+    		// move down
+        	col = initPos.getColumn();
+        	row = initPos.getRow();
+    		newPos = new Position(col, --row);
+    		if(isMoveValid(newPos, state) && state.getPieceAt(newPos) == null ) {
+    			positions.add(newPos);
+    			if(row == 6) {
+    				newPos = new Position(col, --row);
+    				if(isMoveValid(newPos, state) && state.getPieceAt(newPos) == null ) {
+    					positions.add(newPos);
+    				}
+    			}
+    		}
+
+    		// move down left
+        	col = initPos.getColumn();
+        	row = initPos.getRow();
+    		newPos = new Position(--col, --row);
+    		if(isMoveValid(newPos, state) && state.getPieceAt(newPos) != null) {
+    			positions.add(newPos);
+    		}
+
+    		// move down right
+        	col = initPos.getColumn();
+        	row = initPos.getRow();
+    		newPos = new Position(++col, --row);
+    		if(isMoveValid(newPos, state) && state.getPieceAt(newPos) != null) {
+    			positions.add(newPos);
+    		}
+    	}
+
+    	return positions;
+    }   
 }
