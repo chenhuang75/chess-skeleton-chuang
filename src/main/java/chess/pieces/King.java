b935cb9d1ee6e2efbf01fa0f53fb45a639786eef
chenhuang75 <chen.huang.c@gmail.com>
2015-12-05 14:29:41 -0500

    Update King.java

--- src/main/java/chess/pieces/King.java
+++ src/main/java/chess/pieces/King.java
@@ -1,6 +1,11 @@
 package chess.pieces;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import chess.GameState;
 import chess.Player;
+import chess.Position;
 
 /**
  * The King class
@@ -14,4 +19,66 @@ public class King extends Piece {
     protected char getIdentifyingCharacter() {
         return 'k';
     }
+    
+    /** 
+     * get possible next positions based on the game
+     */
+    public List<Position> getNextPositions(Position initPos, GameState state) {
+    	char col = initPos.getColumn();
+    	int row = initPos.getRow();
+    	List<Position> positions = new ArrayList<Position>();
+    	Position newPos;
+    	
+    	boolean a = state.areKingsAdjacent();
+    	
+    	// move up
+    	newPos = new Position(col, row + 1);
+    	if(isMoveValid(newPos, state) ) {
+    		positions.add(newPos);
+    	}
+
+    	// move down
+    	newPos = new Position(col, row - 1);
+    	if(isMoveValid(newPos, state) ) {
+    		positions.add(newPos);
+    	}
+
+    	// move left
+    	newPos = new Position((char)(col - 1), row);
+    	if(isMoveValid(newPos, state) ) {
+    		positions.add(newPos);
+    	}
+
+    	// move right
+    	newPos = new Position((char)(col + 1), row);
+    	if(isMoveValid(newPos, state) ) {
+    		positions.add(newPos);
+    	}
+
+    	// move up right
+    	newPos = new Position((char)(col + 1), row + 1);
+    	if(isMoveValid(newPos, state) ) {
+    		positions.add(newPos);
+    	}
+
+    	// move up left
+    	newPos = new Position((char)(col - 1), row + 1);
+    	if(isMoveValid(newPos, state) ) {
+    		positions.add(newPos);
+    	}
+
+    	// move down right
+    	newPos = new Position((char)(col + 1), row - 1);
+    	if(isMoveValid(newPos, state) ) {
+    		positions.add(newPos);
+    	}
+
+    	// move down left
+    	newPos = new Position((char)(col - 1), row - 1);
+    	if(isMoveValid(newPos, state) ) {
+    		positions.add(newPos);
+    	}
+    	
+    	return positions;
+    }   
 }
