797fdb60ffa6b341bd108a02b0585937aeea9c7a
chenhuang75 <chen.huang.c@gmail.com>
2015-12-05 14:29:17 -0500

    Update Bishop.java

--- src/main/java/chess/pieces/Bishop.java
+++ src/main/java/chess/pieces/Bishop.java
@@ -1,6 +1,11 @@
 package chess.pieces;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import chess.GameState;
 import chess.Player;
+import chess.Position;
 
 /**
  * The 'Bishop' class
@@ -14,4 +19,60 @@ public class Bishop extends Piece {
     protected char getIdentifyingCharacter() {
         return 'b';
     }
+
+    /**
+     * get all possible next positions
+     */
+    public List<Position> getNextPositions(Position initPos, GameState state) {
+    	char col;
+    	int row;
+    	List<Position> positions = new ArrayList<Position>();
+    	Position newPos;
+
+    	// move up right
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(++col, ++row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+    		newPos = new Position(++col, ++row);
+    	}
+
+    	// move up left
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(--col, ++row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(--col, ++row);
+    	}
+
+    	// move down right
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(++col, --row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(++col, --row);
+    	}
+
+    	// move down left
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(--col, --row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(--col, --row);
+    	}
+
+    	return positions;
+    }   
 }
