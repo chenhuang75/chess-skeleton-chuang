1530ab3527f5544e8c4874adc1066e4c040ef292
chenhuang75 <chen.huang.c@gmail.com>
2015-12-05 14:31:36 -0500

    Update Rook.java

--- src/main/java/chess/pieces/Rook.java
+++ src/main/java/chess/pieces/Rook.java
@@ -1,6 +1,11 @@
 package chess.pieces;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import chess.GameState;
 import chess.Player;
+import chess.Position;
 
 /**
  * The 'Rook' class
@@ -15,4 +20,60 @@ public class Rook extends Piece {
     protected char getIdentifyingCharacter() {
         return 'r';
     }
+
+    /**
+     * get all possible next positions
+     */
+    public List<Position> getNextPositions(Position initPos, GameState state) {
+    	char col;
+    	int row;
+    	List<Position> positions = new ArrayList<Position>();
+    	Position newPos;
+    	
+    	// move up
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(col, ++row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(col, ++row);
+    	}
+
+    	// move down
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(col, --row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(col, --row);
+    	}
+
+    	// move left
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(--col, row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(--col, row);
+    	}
+
+    	// move right
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(++col, row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(++col, row);
+    	}
+
+    	return positions;
+    }   
 }
