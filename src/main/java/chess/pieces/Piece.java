22deb318ab4091cf2bcb66a234905b78af732127
chenhuang75 <chen.huang.c@gmail.com>
2015-12-05 14:30:52 -0500

    Update Piece.java

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -1,6 +1,10 @@
 package chess.pieces;
 
+import java.util.List;
+
+import chess.GameState;
 import chess.Player;
+import chess.Position;
 
 /**
  * A base class for chess pieces
@@ -26,4 +30,16 @@ public abstract class Piece {
     }
 
     protected abstract char getIdentifyingCharacter();
+
+    public abstract List<Position> getNextPositions(Position initPos, GameState state);
+    
+    /**
+     * check if a move is valid
+     * @param newPos
+     * @param state
+     * @return
+     */
+    protected boolean isMoveValid(Position newPos, GameState state) {
+    	return (newPos.isvalid() && (state.getPieceAt(newPos) == null || state.getPieceAt(newPos).getOwner() != this.getOwner()));
+    }
 }
