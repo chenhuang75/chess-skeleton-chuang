d321bd101304674a4265a7ad8ca62c19190c7261
chenhuang75 <chen.huang.c@gmail.com>
2015-12-05 14:31:16 -0500

    Update Queen.java

--- src/main/java/chess/pieces/Queen.java
+++ src/main/java/chess/pieces/Queen.java
@@ -1,6 +1,11 @@
 package chess.pieces;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import chess.GameState;
 import chess.Player;
+import chess.Position;
 
 /**
  * The Queen
@@ -14,4 +19,104 @@ public class Queen extends Piece{
     protected char getIdentifyingCharacter() {
         return 'q';
     }
+
+    /**
+     * get all possible next positions
+     */
+    public List<Position> getNextPositions(Position initPos, GameState state) {
+    	char col;
+    	int row;
+    	List<Position> positions = new ArrayList<Position>();
+    	Position newPos;
+    	
+    	// move up
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(col, ++row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(col, ++row);
+    	}
+
+    	// move down
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(col, --row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(col, --row);
+    	}
+
+    	// move left
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(--col, row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(--col, row);
+    	}
+
+    	// move right
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(++col, row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(++col, row);
+    	}
+
+    	// move up right
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(++col, ++row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(++col, ++row);
+    	}
+
+    	// move up left
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(--col, ++row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(--col, ++row);
+    	}
+
+    	// move down right
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(++col, --row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(++col, --row);
+    	}
+
+    	// move down left
+    	col = initPos.getColumn();
+    	row = initPos.getRow();
+    	newPos = new Position(--col, --row);
+    	while(isMoveValid(newPos, state)) {
+    		positions.add(newPos);
+    		if( state.getPieceAt(newPos) != null)
+    			break;
+        	newPos = new Position(--col, --row);
+    	}
+
+    	return positions;
+    }   
 }
